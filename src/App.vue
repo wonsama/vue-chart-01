fc6d0602e6a9ab6ad419c280e7d5861e01d02242
park won <parkwon@impconsulting.co.kr>
2022-06-21 14:49:20 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Animation from "./components/Animation.vue";
+import AutomaticLayout from "./components/AutomaticLayout.vue";
+import Basic from "./components/Basic.vue";
+</script>
+
+<template>
+  <AutomaticLayout />
+</template>
+
+<style>
+.v-network-graph {
+  width: 100%;
+  height: 600px;
+}
+</style>
